4f74b11ac0122f1d30f7655499405f4c6aa8bdb7
wudixiaochongzi <1150964664@qq.com>
2016-04-27 23:46:23 +0800

    用swiper套轮播

--- js/index.js
+++ js/index.js
@@ -2,7 +2,21 @@ $(function(){
     var $btn=$('.logo-box .btn');
     var $max=$('.max-header');
     $btn.click(function(){
-        //$max.animate({height:300});
+        $max.stop();
         $max.slideToggle();
-    })
+        //$max.css({height:'auto'});
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false,
+        loop: true
+    });
 });
